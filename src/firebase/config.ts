0ec98f4c71fe451ad31b8091220c8ddffc4ff11c
Nikhila <23102160@rmd.ac.in>
2026-04-01 08:46:18 +0530

    Add files via upload

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5261467939-a8c1c",
+  "appId": "1:232955712361:web:0971a6af2ca6331494e3f8",
+  "apiKey": "AIzaSyBLDow4mM4cCcZkwxqXZio1EZfb0hSG1Rg",
+  "authDomain": "studio-5261467939-a8c1c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "232955712361"
+};
